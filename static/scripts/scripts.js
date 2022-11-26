b100ca2600f85af43f1ae8a06376c501715a7e43
Putu Widyantara <putuwaw973@gmail.com>
2022-11-27 02:25:50 +0800

    Create function to validate user input

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,1 +1,14 @@
-// scripts.js
+/**
+ * Validate user input
+ * @return true if user input is accepted and false otherwise
+ */
+function validate_form() {
+  let inputValue = document.getElementById("string").value;
+  for (let i = 0; i < inputValue.length; i++) {
+    if (inputValue[i] != "a" && inputValue[i] != "b") {
+      alert("String tidak valid! Hanya diperbolehkan kombinasi a dan/atau b!");
+      return false;
+    }
+  }
+  return true;
+}
